4a004207cfc9b3b0c49f92f1a6c733eff3a7252a
matancarmeli7 <matan.carmeli7@gmail.com>
2022-01-30 11:36:57 +0200

    update ibm copy writes
    Signed-off-by: matancarmeli7 <matan.carmeli7@gmail.com>

--- plugins/modules/ds8000_host.py
+++ plugins/modules/ds8000_host.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 
-# (c) 2021, Matan Carmeli <matan.carmeli7@gmail.com>
+# Copyright (C) 2021 IBM CORPORATION
+# Author(s): Matan Carmeli <matan.carmeli7@gmail.com>
+#
 # GNU General Public License v3.0+ (see COPYING or https://www.gnu.org/licenses/gpl-3.0.txt)
 
 from __future__ import absolute_import, division, print_function
